adedce10a482eacdf2cbe7ae9b79cc69125637d9
angel <sortonesangel@gmail.com>
2025-05-08 14:05:57 +0800

    Updates

--- assets/ios.transition-legacy-CS0WstMM.js
+++ assets/ios.transition-legacy-CS0WstMM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DQKPKJrs.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CrDJvfRb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-YGVpqfgO.js
+++ assets/keyboard2-legacy-YGVpqfgO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DQKPKJrs.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CrDJvfRb.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
