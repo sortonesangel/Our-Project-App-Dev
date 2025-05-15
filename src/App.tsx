19d749fc7f2cf8cea4e4589ebcb59d9ffe7d01b9
jamlagz28 <jameslaag2003@gmail.com>
2025-05-15 14:47:55 +0800

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,38 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Menu from './pages/Menu';
-
+/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
+
+/* Theme variables */
 import './theme/variables.css';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+import Register from './pages/Register';
 
 setupIonicReact();
 
@@ -25,13 +41,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        {/* Login/Register routes */}
-        <Route exact path="/" render={() => <Redirect to="/our-project-app-dev" />} />
         <Route exact path="/our-project-app-dev" component={Login} />
-        <Route exact path="/our-project-app-dev/login" component={Login} />
         <Route exact path="/our-project-app-dev/register" component={Register} />
-
-        {/* App layout with Menu */}
         <Route path="/our-project-app-dev/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
